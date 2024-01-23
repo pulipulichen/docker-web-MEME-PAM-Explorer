70778eb264be01ec67176a6ecbf0fb7598940725
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-01-24 00:48:24 +0800

    西元2024年01月24日 (週三) 00時48分24秒 CST

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+FROM solr:8.7.0
+
+USER root
+
+RUN apt-get update
+RUN apt-get install -y curl wget nano rsync mlocate vim
+RUN updatedb
+RUN apt-get install -y pip
+RUN pip install pyexcel==0.7.0 pyexcel-ods==0.6.0 pandas==2.1.2 requests==2.31.0
+
+#RUN sed -i '/jessie-updates/d' /etc/apt/sources.list
+
+# RUN apt-get update
+# RUN apt-get install -y wget nano curl wget rsync
+
+#RUN mkdir -p /docker-build/
+#RUN chown solr:solr -R /docker-build/
+
+ENV LOCAL_VOLUMN_PATH=/var/solr/data/collection/conf/
+#ENV SHARED_PATH=/opt/solr-9.4.0/
+ENV LOCAL_PORT=8983
+
+#RUN mv /opt/solr-9.4.0 /opt/solr-9.4.0-original
+#RUN chown solr:solr -R /opt/solr-9.4.0-original
+
+#RUN mkdir -p /opt/solr-9.4.0
+#RUN chown solr:solr -R /opt/solr-9.4.0
+
+ENTRYPOINT []
+CMD ["bash", "/startup.sh"]
+
+#USER solr
+
+RUN mkdir -p /var/solr/data/collection/conf
+RUN echo "name=collection" > /var/solr/data/collection/core.properties
+RUN mkdir -p /docker-build/conf
+COPY ./docker-build/jetty.xml /opt/solr-8.7.0/server/etc/jetty.xml
+COPY ./docker-build/conf /docker-build/conf
+COPY ./docker-build/python /docker-build/python
+RUN chmod -R 777 "/docker-build/conf/"
+COPY ./docker-build/console.sh /console.sh
+COPY ./docker-build/startup.sh /startup.sh
+
+# CMD ["solr-foreground", "-force"]
+# RUN echo "20231101-0056"
---
+++ docker-build/python/init_data.py
@@ -0,0 +1,73 @@
+import requests
+import csv
+import json
+import os
+
+local_volume_path = os.environ.get('LOCAL_VOLUMN_PATH')
+input_file = os.path.join(local_volume_path, 'data.csv')
+temp_file = os.path.join(local_volume_path, 'data-temp.csv')
+
+def is_url(url):
+    return url.startswith("http://") or url.startswith("https://")
+
+def is_google_sheet_url(url):
+    return url.startswith("https://docs.google.com/spreadsheets/d/")
+
+def convert_google_sheet_url_to_csv(url):
+    if url.endswith("/export?format=csv"):
+        return url
+    
+    if "/edit" in url:
+        base_url, query_params = url.split('/edit')
+        csv_export_url = f"{base_url}/export?format=csv"
+        return csv_export_url
+    elif "/export" in url:
+        base_url, query_params = url.split('/export')
+        csv_export_url = f"{base_url}/export?format=csv"
+        return csv_export_url
+    else:
+        return None
+
+def download_file(url, output_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(output_path, 'wb') as file:
+            file.write(response.content)
+        return True
+    return False
+
+def convert_json_to_csv(json_data, output_path):
+    if json_data:
+        keys = list(json_data[0].keys())
+        with open(output_path, 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=keys)
+            writer.writeheader()
+            writer.writerows(json_data)
+
+def main():
+
+    if os.path.isfile(input_file):
+        with open(input_file, 'r') as file:
+            url = file.read().strip()
+
+        if is_url(url):
+            if is_google_sheet_url(url):
+                url = convert_google_sheet_url_to_csv(url)
+                print(url)
+
+            if download_file(url, temp_file):
+                with open(temp_file, 'r') as file:
+                    try:
+                        json_data = json.load(file)
+                        convert_json_to_csv(json_data, temp_file)
+                    except json.JSONDecodeError:
+                        pass
+            else:
+                print("Failed to download the file from the URL.")
+        else:
+            print("The file does not contain a valid URL.")
+    else:
+        print("File not found.")
+
+if __name__ == "__main__":
+    main()
